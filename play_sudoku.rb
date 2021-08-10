8e3b0918b6d429eb4f38d85a1cc38fa7fcd17c1c
FS135 <florian.schulte02@gmx.de>
2021-08-10 13:18:04 +0200

    made style changeable

--- play_sudoku.rb
+++ play_sudoku.rb
@@ -2,6 +2,7 @@ require "byebug"
 require_relative "sudoku"
 
 sudoku = Sudoku.new
+
 puts "\e[H\e[2J"
 puts "\n" * 1
 
@@ -12,11 +13,9 @@ while true
   
   puts "\n" * 2
   
-  puts "hi"
-  
   puts "geben sie eine Zahl ein mit den Koordinaten xyn"
   input = gets.chomp
-  number = input.to_s[2].to_i
+  number = input.to_s[2]
   position = [input.to_s[0].to_i, input.to_s[1].to_i]
   puts "\e[H\e[2J"
   puts "\n" * 1
--- sudoku.rb
+++ sudoku.rb
@@ -2,75 +2,99 @@ require "byebug"
 class Sudoku
 
   attr_reader :num_arr
+  attr_accessor :field, :sudoku
+  @@field = "default"
+  @@sudoku = "default"
+  @@top_offset = "\n" * 2
+  @@left_offset = " " * 6
+  @@highlighting = true
+  @@characters = [1,2,3,4,5,6,7,8,9]
 
-  DEMO_SUDOKU = "530070000600195000098000060800060003400803001700020006060000280000419005000080079"
-  TOP_OFFSET = "\n" * 2
-  LEFT_OFFSET = " " * 6
-
-  def self.load_sudoku(str=DEMO_SUDOKU)
-    arr = [[[],[],[]],[[],[],[]],[[],[],[]],[[],[],[]],[[],[],[]],[[],[],[]],[[],[],[]],[[],[],[]],[[],[],[]]]
-    (0..8).each do |x|
-      (0..2).each do |y|
-        (0..2).each do |z|
-          if str[ x * 9 + y * 3 + z ] == "0"
-            arr[x][y][z] = " "
-          else
-            arr[x][y][z] = str[ x * 9 + y * 3 + z ].to_i
-          end
-        end
-      end
-    end
-    arr
+  def self.load_sudoku(sudokuname: @@sudoku)
+    require_relative "sudokus/" + sudokuname
+    sudoku
   end
 
-  def initialize
-    @num_arr = Sudoku.load_sudoku
+  def self.load_field(fieldname: @@field)
+    require_relative "fields/" + fieldname
+    field
+  end
+
+  def initialize(sudokuname: @@sudoku, fieldname: @@field)
+    puts sudokuname
+    puts fieldname
+    @sudoku_arr = Sudoku.load_sudoku(sudokuname: sudokuname)
+    @field_arr = Sudoku.load_field(fieldname: fieldname)
+    @@highlighting ? @highlighted = [5,5] : @highlighted = [nil,nil]
+  end
+
+  def update
+    puts @@field
+    @sudoku_arr = Sudoku.load_sudoku
+    @field_arr = Sudoku.load_field
   end
 
   def draw
-    puts TOP_OFFSET
-    @num_arr.each_with_index do |ele,idx|
-      puts LEFT_OFFSET+" #{ele[0][0]} | #{ele[0][1]} | #{ele[0][2]} || #{ele[1][0]} | #{ele[1][1]} | #{ele[1][2]} || #{ele[2][0]} | #{ele[2][1]} | #{ele[2][2]} "
-      if (idx + 1) % 3 != 0
-        puts LEFT_OFFSET + "---+---+---||---+---+---||---+---+---"
-      elsif idx == 2
-        puts LEFT_OFFSET + "========================||==========="
-      elsif idx == 5
-        puts LEFT_OFFSET + "===========||========================"
+    puts @@top_offset
+    x = 0
+    y = 0
+    fieldt = []
+
+    @field_arr.each do |line|
+      linet = ""
+      line.each_char do |char|
+        if char == "&"
+          linet += @sudoku_arr[x][y]
+          y!=8 ? y+=1 : (y=0 and x+=1)
+        else
+          linet += char
+        end
       end
+      fieldt.push(linet)
     end
+
+    fieldt.each { |line| puts line }
   end
 
-  def num_in_line?(num, pos)
-    if @num_arr[pos[0]].flatten.include?(num)
+  def num_in_line?(num,x,y)
+    if @sudoku_arr[x].include?(num)
       return true
     end
     false
   end
 
-  def num_in_col?(num, pos)
-    (0..8).each do |x|
-      if @num_arr[x].flatten[pos[1]] == num
+  def num_in_col?(num,x,y)
+    (0..8).each do |i|
+      if @sudoku_arr[i][y] == num
         return true
       end
     end
     false
   end
 
-  def num_in_field?(num, pos)
-    f_start = pos[0] / 3 * 3
-    f_end = f_start + 2
-    (f_start..f_end).each { |i| return true if @num_arr[i][pos[1]/3].include?(num) }
+  def num_in_field?(num,x,y)
+    l_start = x / 3 * 3
+    l_end = l_start + 2
+    n_start = y / 3 * 3
+    n_end = n_start + 2
+    (l_start..l_end).each { |i| return true if @sudoku_arr[i][n_start..n_end].include?(num) }
     false
   end
 
-  def try_num(num, pos)
-    pos_i = [pos[0] - 1, pos[1] - 1]
-    return "num in line!" if num_in_line?(num,pos_i)
-    return "num in column!" if num_in_col?(num,pos_i)
-    return "num in field!" if num_in_field?(num,pos_i)
-    @num_arr[pos_i[0]][pos_i[1]/3][pos_i[1]%3] = num
-    true
+  def try_num(num, pos=@highlighted)
+    x = pos[0] - 1
+    y = pos[1] - 1
+    num_in_line = num_in_line?(num,x,y)
+    num_in_col = num_in_col?(num,x,y)
+    num_in_field = num_in_field?(num,x,y)
+    puts "num in line:\t" + num_in_line.to_s
+    puts "num in column:\t" + num_in_col.to_s
+    puts "num in field:\t" + num_in_field.to_s
+    if !num_in_line && !num_in_col && !num_in_field
+      @sudoku_arr[x][y] = num
+      return "number might be correct"
+    end
+    "number is wrong"
   end
 
 end
